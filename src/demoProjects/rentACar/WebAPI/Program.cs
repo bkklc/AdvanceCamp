3cd534e5afde51cb8914792781bc963610a2864a
bkklc <abkklc@gmail.com>
2024-01-30 00:46:07 +0300

    Create Layers, Add Migration

--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -1,11 +1,15 @@
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddApplicationServices();
+//builder.Services.AddSecurityServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
+//builder.Services.AddInfrastructureServices();
+//builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
